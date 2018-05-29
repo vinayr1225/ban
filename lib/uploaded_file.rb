cf60bdb65268233b85f568799e04f67d1bac4342
Alessio Caiazza <acaiazza@gitlab.com>
2018-05-29 13:48:23 +0200

    Disable local copy of files with direct_upload
    When workhorse writes files directly to object storage, there's no need
    to also write it on disk

--- lib/uploaded_file.rb
+++ lib/uploaded_file.rb
@@ -18,16 +18,23 @@ class UploadedFile
 
   attr_reader :remote_id
   attr_reader :sha256
+  attr_reader :size
 
-  def initialize(path, filename: nil, content_type: "application/octet-stream", sha256: nil, remote_id: nil)
-    raise InvalidPathError, "#{path} file does not exist" unless ::File.exist?(path)
+  def initialize(path, filename: nil, content_type: "application/octet-stream", sha256: nil, remote_id: nil, size: nil)
+    if remote_id.blank?
+      raise InvalidPathError, "#{path} file does not exist" unless ::File.exist?(path)
+
+      @tempfile = File.new(path, 'rb')
+      @size = @tempfile.size
+    else
+      @size = size
+    end
 
     @content_type = content_type
     @original_filename = sanitize_filename(filename || path)
     @content_type = content_type
     @sha256 = sha256
     @remote_id = remote_id
-    @tempfile = File.new(path, 'rb')
   end
 
   def self.from_params(params, field, upload_paths)
@@ -37,18 +44,25 @@ class UploadedFile
       return
     end
 
-    file_path = File.realpath(params["#{field}.path"])
+    file_path = nil
 
-    paths = Array(upload_paths) << Dir.tmpdir
-    unless self.allowed_path?(file_path, paths.compact)
-      raise InvalidPathError, "insecure path used '#{file_path}'"
+    if params["#{field}.path"]
+      file_path = File.realpath(params["#{field}.path"])
+
+      paths = Array(upload_paths) << Dir.tmpdir
+      unless self.allowed_path?(file_path, paths.compact)
+        raise InvalidPathError, "insecure path used '#{file_path}'"
+      end
+    else
+      raise InvalidPathError, "file is invalid" if params["#{field}.remote_id"].blank?
     end
 
     UploadedFile.new(file_path,
       filename: params["#{field}.name"],
       content_type: params["#{field}.type"] || 'application/octet-stream',
       sha256: params["#{field}.sha256"],
-      remote_id: params["#{field}.remote_id"])
+      remote_id: params["#{field}.remote_id"],
+      size: params["#{field}.size"])
   end
 
   def self.allowed_path?(file_path, paths)
