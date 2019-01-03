63a1e32dad9ccb59bdb3f65b4619b96317e9cd25
James Lopez <james@jameslopez.es>
2019-01-03 15:46:12 +0100

    Add alex as a Danger file
    Adds a new step in Danger to warn on non-inclusive and profane language.
    
    This affects:
    
    - Commits
    - Documentation

---
+++ danger/alex/alex_command.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/gitlab/popen'
+
+class AlexCommand
+  Result = Struct.new(:stdout, :stderr, :status)
+
+  def self.from_paths(paths)
+    execute(%W[alex #{paths.join(' ')}])
+  end
+
+  def self.from_text(text)
+    execute(%w[alex --stdin], text)
+  end
+
+  def self.execute(cmd, input = nil)
+    Open3.popen3({}, *cmd) do |stdin, stdout, stderr, wait_thr|
+      stdin.puts(input)
+      stdin.flush
+
+      # stderr and stdout pipes can block if stderr/stdout aren't drained: https://bugs.ruby-lang.org/issues/9082
+      # Mimic what Ruby does with capture3: https://github.com/ruby/ruby/blob/1ec544695fa02d714180ef9c34e755027b6a2103/lib/open3.rb#L257-L273
+      out_reader = Thread.new { stdout.read }
+      err_reader = Thread.new { stderr.read }
+
+      yield(stdin) if block_given?
+      stdin.close
+
+      Result.new(out_reader.value, err_reader.value, wait_thr.value)
+    end
+  end
+
+  private_class_method :execute
+end
