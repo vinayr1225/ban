008877f682c565c6d7945b2c5ae575020ea9038b
Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>
2019-03-20 11:46:38 +0200

    Replace response.to_s with response.body.to_s
    Signed-off-by: Dmitriy Zaporozhets <dmitriy.zaporozhets@gmail.com>

--- app/services/web_hook_service.rb
+++ app/services/web_hook_service.rb
@@ -41,7 +41,7 @@ class WebHookService
     {
       status: :success,
       http_status: response.code,
-      message: response.to_s
+      message: response.body.to_s
     }
   rescue SocketError, OpenSSL::SSL::SSLError, Errno::ECONNRESET, Errno::ECONNREFUSED, Errno::EHOSTUNREACH, Net::OpenTimeout, Net::ReadTimeout, Gitlab::HTTP::BlockedUrlError, Gitlab::HTTP::RedirectionTooDeep => e
     log_execution(
