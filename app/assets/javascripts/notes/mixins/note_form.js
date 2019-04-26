a8b04233d970575a460864aa3fb882a37b2c0348
Sam Bigelow <sbigelow@gitlab.com>
2019-04-26 10:43:50 -0400

    EE Backport to CE of EE!11151
    This moves all EE Specific lines into the ee directory

---
+++ app/assets/javascripts/notes/mixins/note_form.js
@@ -0,0 +1,19 @@
+export default {
+  methods: {
+    handleKeySubmit() {
+      this.handleUpdate();
+    },
+    handleUpdate(shouldResolve) {
+      const beforeSubmitDiscussionState = this.discussionResolved;
+      this.isSubmitting = true;
+
+      this.$emit('handleFormUpdate', this.updatedNoteBody, this.$refs.editNoteForm, () => {
+        this.isSubmitting = false;
+
+        if (this.shouldToggleResolved(shouldResolve, beforeSubmitDiscussionState)) {
+          this.resolveHandler(beforeSubmitDiscussionState);
+        }
+      });
+    },
+  },
+};
