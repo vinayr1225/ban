978e6a44bd6fa630fd0ce284c434ce34d2cc4bf1
Alessio Caiazza <acaiazza@gitlab.com>
2019-05-17 15:54:43 +0200

    Avoid ActiveRecord in loader

--- lib/gitlab/graphql/loaders/namespace_statistics_loader.rb
+++ lib/gitlab/graphql/loaders/namespace_statistics_loader.rb
@@ -8,15 +8,13 @@ module Gitlab
           @namespace_id = namespace_id
         end
 
-        # rubocop: disable CodeReuse/ActiveRecord
         def find
           BatchLoader.for(namespace_id).batch do |ids, loader|
-            results = Namespace.with_statistics.where(id: ids)
+            results = Namespace.with_statistics.find(ids)
 
             results.each { |namespace| loader.call(namespace.id, namespace) }
           end
         end
-        # rubocop: enable CodeReuse/ActiveRecord
 
         private
 
