d353637c636d9b4989d3ea391b7b3eb3b8a3ba24
Luke Duncalfe <lduncalfe@eml.cc>
2019-07-01 16:34:34 +1200

    GraphQL mutations for managing Notes
    https://gitlab.com/gitlab-org/gitlab-ce/issues/62826

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -10,5 +10,10 @@ module Types
     mount_mutation Mutations::AwardEmojis::Remove
     mount_mutation Mutations::AwardEmojis::Toggle
     mount_mutation Mutations::MergeRequests::SetWip, calls_gitaly: true
+    mount_mutation Mutations::Notes::Create::Note
+    mount_mutation Mutations::Notes::Create::DiffNote
+    mount_mutation Mutations::Notes::Create::ImageDiffNote
+    mount_mutation Mutations::Notes::Update
+    mount_mutation Mutations::Notes::Destroy
   end
 end
