16cbda053fe27de735a54120ba322fa0eeab892f
Alex Kalderimis <alex.kalderimis@gmail.com>
2019-08-22 23:08:09 +0100

    Use next rather than return to return early from a block

--- db/fixtures/development/24_forks.rb
+++ db/fixtures/development/24_forks.rb
@@ -3,12 +3,12 @@ require './spec/support/sidekiq'
 Sidekiq::Testing.inline! do
   Gitlab::Seeder.quiet do
     User.all.sample(10).each do |user|
-      source_project = Project.public_only.sample
+      source_project = Project.public_only.where.not(creator: user).sample
 
       ##
       # 04_project.rb might not have created a public project because
       # we use randomized approach (e.g. `Array#sample`).
-      return unless source_project
+      next unless source_project
 
       fork_project = Projects::ForkService.new(source_project, user, namespace: user.namespace).execute
 
