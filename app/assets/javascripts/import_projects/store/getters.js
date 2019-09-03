2dd59c8885ee9fafbda60170409ca2c71f74b2c3
George Koltsov <gkoltsov@gitlab.com>
2019-09-03 16:17:55 +0100

    Add GitHub & Gitea importer projects filtering
    Backend:
      - Add new filter param
      - Add sanitization of filter param
      - Add actual filtering of repos/projects collection
    
    Frontend:
      - Add new input field on the UI
      - Add new filter to Import Projects Table state
      - Add realtime changes endpoint filtering
      - Add throttling of filtering http requests
    
    - Update documentation & screenshots

--- app/assets/javascripts/import_projects/store/getters.js
+++ app/assets/javascripts/import_projects/store/getters.js
@@ -20,3 +20,14 @@ export const isImportingAnyRepo = state => state.reposBeingImported.length > 0;
 export const hasProviderRepos = state => state.providerRepos.length > 0;
 
 export const hasImportedProjects = state => state.importedProjects.length > 0;
+
+export const concatenatedPath = (path, filter) => {
+  if (filter && filter.length > 0) {
+    return path.concat(`?filter=${filter}`);
+  }
+
+  return path;
+};
+
+export const reposPathWithFilter = state => concatenatedPath(state.reposPath, state.filter);
+export const jobsPathWithFilter = state => concatenatedPath(state.jobsPath, state.filter);
