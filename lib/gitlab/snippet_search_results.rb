b0c8f2b1ac86de681befb92a42cd629e57c2747b
Francisco Javier López <fjlopez@gitlab.com>
2019-09-12 15:17:32 +0200

    Adding limit and avoid reordering in count queries

--- lib/gitlab/snippet_search_results.rb
+++ lib/gitlab/snippet_search_results.rb
@@ -3,6 +3,7 @@
 module Gitlab
   class SnippetSearchResults < SearchResults
     include SnippetsHelper
+    include Gitlab::Utils::StrongMemoize
 
     attr_reader :limit_snippets
 
@@ -25,33 +26,37 @@ module Gitlab
     def formatted_count(scope)
       case scope
       when 'snippet_titles'
-        snippet_titles_count.to_s
+        formatted_limited_count(limited_snippet_titles_count)
       when 'snippet_blobs'
-        snippet_blobs_count.to_s
+        formatted_limited_count(limited_snippet_blobs_count)
       else
         super
       end
     end
 
-    def snippet_titles_count
-      @snippet_titles_count ||= snippet_titles.count
+    def limited_snippet_titles_count
+      strong_memoize(:limited_snippet_titles_count) do
+        limited_count(snippet_titles)
+      end
     end
 
-    def snippet_blobs_count
-      @snippet_blobs_count ||= snippet_blobs.count
+    def limited_snippet_blobs_count
+      strong_memoize(:limited_snippet_blobs_count) do
+        limited_count(snippet_blobs)
+      end
     end
 
     private
 
     # rubocop: disable CodeReuse/ActiveRecord
     def snippet_titles
-      limit_snippets.search(query).order('updated_at DESC').includes(:author)
+      limit_snippets.search(query).reorder('updated_at DESC').includes(:author)
     end
     # rubocop: enable CodeReuse/ActiveRecord
 
     # rubocop: disable CodeReuse/ActiveRecord
     def snippet_blobs
-      limit_snippets.search_code(query).order('updated_at DESC').includes(:author)
+      limit_snippets.search_code(query).reorder('updated_at DESC').includes(:author)
     end
     # rubocop: enable CodeReuse/ActiveRecord
 
